33b91c1597a96402c8197042db3fceb104acfc4f
arnau <arnaubennassar5@gmail.com>
2018-10-08 22:32:27 +0200

    quick fix

--- osmc/module/next_cloud.py
+++ osmc/module/next_cloud.py
@@ -1,4 +1,7 @@
 import os
 
 def install():
-    os.system("curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | bash")
+    os.system("sudo curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | bash")
+    return """
+    ----- NextCloudPi:
+    """
