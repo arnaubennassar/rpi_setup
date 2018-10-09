b10a3ff6f2bb845c75ad82eec2ddb095f5fbb12d
arnau <arnaubennassar5@gmail.com>
2018-10-09 14:53:48 +0200

    refactor

--- config_template.py
+++ config_template.py
@@ -4,18 +4,18 @@ movies_dir = 'media/DISK/MOVIES'
 series_dir = 'media/DISK/SERIES'
 change_pass = 'yes'
 
-install_spotify = 'yes'
+install_spotify = 'yes' #or 'no'
 spotify_name = 'habita'
 
 transmission_user = 'osmc'
 transmission_pass = 'formen5hongkong'
 
-install_sickrage = 'yes'
+install_sickrage = 'yes' #or 'no'
 sickrage_name = 'osmc'
-sickrage_pass = 'formen5hongkong'
+sickrage_pass = '***********'
 
-trakt_user = 'arnaubennassar5@gmail.com'
+trakt_user = 'email of your trakt account'
 
-install_potato = 'yes'
+install_potato = 'yes' #or 'no'
 
-install_nextcloudpi = 'yes'
+install_nextcloudpi = 'yes' #or 'no'
