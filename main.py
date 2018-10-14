0837492419e7f1b01aa97411672441433dd60b74
arnau <arnaubennassar5@gmail.com>
2018-10-14 17:42:54 +0200

    added flexget

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ if c.install_flexget == 'yes':
     os.system("mkdir ~/flexget")
     os.system("cp module/flexget/secrets.yml ~/flexget/secrets.yml")
     os.system("cp module/flexget/secrets.yml ~/flexget/config.yml")
-    os.system("cp module/flexget/plugins ~/flexget/plugins")
-    os.systems("cd module/flexget")
-    os.systems("bash autosetup.sh")
+    os.system("cp -r module/flexget/plugins ~/flexget/plugins")
+    os.system("cd module/flexget")
+    os.system("bash autosetup.sh")
     # final_output += sick_rage.install(c.sickrage_name, c.sickrage_pass, c.download_dir, c.series_dir, c.trakt_user, c.transmission_user, c.transmission_pass)
 
 if c.install_sickrage == 'yes':
